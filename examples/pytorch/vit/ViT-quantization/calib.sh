49e124d5a8c946b427b2f9c6db1669425ef1c882
Tony Yi <24402057+tony-ze-yi@users.noreply.github.com>
2023-04-11 19:02:20 -0400

    fix: change to r50 vit

--- examples/pytorch/vit/ViT-quantization/calib.sh
+++ examples/pytorch/vit/ViT-quantization/calib.sh
@@ -2,9 +2,9 @@ python -m torch.distributed.launch --nproc_per_node 1 \
     --master_port 12345 main.py \
     --calib \
     --name vit \
-    --pretrained_dir $CKPT_DIR/ViT-B_16.npz \
+    --pretrained_dir $CKPT_DIR/R50-ViT-B_16.npz \
     --data-path $DATA_DIR \
-    --model_type ViT-B_16 \
+    --model_type R50-ViT-B_16 \
     --img_size 384 \
     --num-calib-batch 10 \
     --calib-batchsz 8 \
--- examples/pytorch/vit/ViT-quantization/qat.sh
+++ examples/pytorch/vit/ViT-quantization/qat.sh
@@ -2,13 +2,13 @@ python -m torch.distributed.launch --nproc_per_node 1 \
     --master_port 12345 main.py \
     --train \
     --name vit \
-    --pretrained_dir calib-checkpoint/ViT-B_16_calib.pth \
+    --pretrained_dir calib-checkpoint/R50-ViT-B_16_calib.pth \
     --data-path $DATA_DIR \
-    --model_type ViT-B_16 \
+    --model_type R50-ViT-B_16 \
     --quant-mode ft2 \
     --img_size 384 \
     --distill \
-    --teacher $CKPT_DIR/ViT-B_16.npz \
+    --teacher $CKPT_DIR/R50-ViT-B_16.npz \
     --output qat_output \
     --quant-mode ft2\
     --batch-size 8 \
