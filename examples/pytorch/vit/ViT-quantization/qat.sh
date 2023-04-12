36a0f3edcb710358f460accd9cf52594f28ab67c
Tony Yi <24402057+tony-ze-yi@users.noreply.github.com>
2023-04-11 22:04:22 -0400

    fix: train batch size

--- examples/pytorch/vit/ViT-quantization/qat.sh
+++ examples/pytorch/vit/ViT-quantization/qat.sh
@@ -7,10 +7,9 @@ python -m torch.distributed.launch --nproc_per_node 1 \
     --model_type R50-ViT-B_16 \
     --quant-mode ft2 \
     --img_size 384 \
-#    --distill \
-#    --teacher $CKPT_DIR/R50-ViT-B_16.npz \
     --output qat_output \
     --quant-mode ft2\
+    --train_batch_size 64\
     --batch-size 24 \
     --num-epochs 100 \
     --qat-lr 1e-3
