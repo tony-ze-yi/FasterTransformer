d49b9ad14e8eb89800d47bd9d82c3fc4ca1f2241
Tony Yi <24402057+tony-ze-yi@users.noreply.github.com>
2023-04-11 21:10:07 -0400

    fix: batch size in training script

--- examples/pytorch/vit/ViT-quantization/qat.sh
+++ examples/pytorch/vit/ViT-quantization/qat.sh
@@ -11,6 +11,6 @@ python -m torch.distributed.launch --nproc_per_node 1 \
 #    --teacher $CKPT_DIR/R50-ViT-B_16.npz \
     --output qat_output \
     --quant-mode ft2\
-    --batch-size 8 \
-    --num-epochs 10 \
+    --batch-size 24 \
+    --num-epochs 100 \
     --qat-lr 1e-3
