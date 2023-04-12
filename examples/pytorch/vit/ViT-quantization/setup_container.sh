9e1a7f5033b35bd801404f61e4ce33a3510a6cbc
momoko <momopoco@protonmail.com>
2023-04-12 05:35:02 -0400

    fix: setup sh

--- examples/pytorch/vit/ViT-quantization/setup_container.sh
+++ examples/pytorch/vit/ViT-quantization/setup_container.sh
@@ -1,2 +1,2 @@
 pip install timm==0.4.12 termcolor==1.1.0 pytorch-quantization ml_collections
-pip install -r examples/pytorch/vit/requirement.txt
+pip install -r /workspace/FasterTransformer/examples/pytorch/vit/requirement.txt
