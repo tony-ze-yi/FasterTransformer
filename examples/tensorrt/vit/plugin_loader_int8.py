287e0633047f3c0722382098d3cc350246a2c4c6
Tony Yi <24402057+tony-ze-yi@users.noreply.github.com>
2023-04-14 22:21:26 -0400

    fix: size for hybrid resnet

--- examples/tensorrt/vit/plugin_loader_int8.py
+++ examples/tensorrt/vit/plugin_loader_int8.py
@@ -44,7 +44,10 @@ class ViTINT8PluginLoader:
 
     def load_model_config(self, config, args):
         print(config)
-        self.patch_size_        = config.patches.size[0]
+        if config.patches.size:
+            self.patch_size_        = config.patches.size[0]
+        else:
+            self.patch_size_        = config.patches.grid[0]
         self.num_heads_         = config.transformer.num_heads
         self.layer_num_         = config.transformer.num_layers
         self.inter_size_        = config.transformer.mlp_dim
